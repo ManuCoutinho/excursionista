965d241fe8b7291ae391a758d35140dc30870d14
ManuCoutinho <gdn.sasha@gmail.com>
2022-03-28 00:19:31 -0300

    updated nort america page

--- src/pages/north-america/index.tsx
+++ src/pages/north-america/index.tsx
@@ -1,28 +1,42 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { Container } from '@chakra-ui/react';
+import { Box, Spinner } from '@chakra-ui/react';
 import { BannerContinent } from '../../components/Continent/Banner';
 import { GridCities } from '../../components/Continent/GridCities';
-import { GridContent } from '../../components/Continent/GridContent';
+
+import bio from '../../utils/continentBio';
+import citiesInfo from '../../utils/citiesInfos';
+import infosBox from '../../utils/infosBox';
 
 const NorthAmerica: NextPage = () => {
-  const bio = `Um continente com poucos países, mas muitas paisagens e cenários diferentes para ver.`;
+  const { northAmerica } = bio;
+  const { northAmerica: continent } = citiesInfo;
+  const { northAmerica: infos } = infosBox;
+
+  const Content = dynamic(() => import('../../components/Continent/GridContent'));
+  const Cities = dynamic(() => import('../../components/Continent/GridCities/GridComponent'), {
+    loading: () => {
+      return <Spinner color='yellow.500' size='lg' />;
+    }
+  });
   return (
     <>
       <Head>
         <title>Excursionista | América do Norte</title>
       </Head>
       <BannerContinent continent='América do Norte' image='northAmerica/usa' color='yellow.500' />
-      <Container maxW='container.lg'>
-        <GridContent text={bio} country='3' language='6' city='10' />
-        <GridCities
-          city='Montreal'
-          country='Canadá'
-          flag='ca'
-          img='/assets/continents/northAmerica/montreal.jpg'
-          url='/'
+      <Box as='section' w={['300', '700', '1024']} p={[4, 6, 12, 16]} mx='auto'>
+        <Content
+          text={northAmerica.bio}
+          country={infos.numberCountry}
+          language={infos.numberLanguage}
+          city={infos.numberTopCity}
         />
-      </Container>
+        <GridCities>
+          <Cities items={continent} />
+        </GridCities>
+      </Box>
     </>
   );
 };
