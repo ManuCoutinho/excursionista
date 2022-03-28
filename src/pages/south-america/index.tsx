5ff84034419c7f75d5cb3c19a61f1ed2e03d3c78
ManuCoutinho <gdn.sasha@gmail.com>
2022-03-28 00:21:57 -0300

    updated south america page

--- src/pages/south-america/index.tsx
+++ src/pages/south-america/index.tsx
@@ -1,32 +1,42 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { Container, Grid } from '@chakra-ui/react';
+import { Box, Spinner } from '@chakra-ui/react';
 import { BannerContinent } from '../../components/Continent/Banner';
 import { GridCities } from '../../components/Continent/GridCities';
-import { GridContent } from '../../components/Continent/GridContent';
+
+import bio from '../../utils/continentBio';
+import citiesInfo from '../../utils/citiesInfos';
+import infosBox from '../../utils/infosBox';
 
 const SouthAmerica: NextPage = () => {
-  const bio = `A cultura sul-americana está presente de diversas maneiras a nível mundial. A cultura indígena 
-  de origem pré-colombiana teve forte influência no Peru, Bolívia e algumas regiões da Amazônia.
-  Por causa da ampla mistura étnica na América do Sul, a culinária tem influências africanas, asiáticas e europeias.
-  O estado brasileiro da Bahia é especialmente conhecido pela influência da culinária da África Ocidental.`;
+  const { southAmerica } = bio;
+  const { southAmerica: continent } = citiesInfo;
+  const { southAmerica: infos } = infosBox;
+
+  const Content = dynamic(() => import('../../components/Continent/GridContent'));
+  const Cities = dynamic(() => import('../../components/Continent/GridCities/GridComponent'), {
+    loading: () => {
+      return <Spinner color='yellow.500' size='lg' />;
+    }
+  });
   return (
     <>
       <Head>
         <title>Excursionista | América do Sul</title>
       </Head>
       <BannerContinent continent='América do Sul' image='latam/latam' color='gray.50' />
-      <Container maxW='container.lg'>
-        <GridContent text={bio} country='12' language='150' city='8' />
-        <Grid as='section' templateColumns='repeat(2, 1fr)' gap={12} my={16}></Grid>
-        <GridCities
-          city='Punta Cana'
-          country='República Dominicana'
-          flag='do'
-          img='/assets/continents/latam/punta.jpg'
-          url='/'
+      <Box as='section' w={['300', '700', '1024']} p={[4, 6, 12, 16]} mx='auto'>
+        <Content
+          text={southAmerica.bio}
+          country={infos.numberCountry}
+          language={infos.numberLanguage}
+          city={infos.numberTopCity}
         />
-      </Container>
+        <GridCities>
+          <Cities items={continent} />
+        </GridCities>
+      </Box>
     </>
   );
 };
